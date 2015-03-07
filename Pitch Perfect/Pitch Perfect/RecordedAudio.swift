1a80edfac75472f42f8659bf1f1d490e81269dd7
Brian <brian.ortega@gmail.com>
2015-03-07 00:58:12 -0500

    Committed by Udacity

---
+++ Pitch Perfect/Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,14 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Brian Ortega on 3/5/15.
+//  Copyright (c) 2015 AZC. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject {
+    var filePathURL: NSURL!
+    var title:String!
+}
